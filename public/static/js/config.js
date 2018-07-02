1d788d7a97870b32bca9df1ba7b0e3ddd5a6ff01
guohao <1403902918@qq.com>
2018-07-03 00:33:04 +0800

    首页 轮播 over

--- public/static/js/config.js
+++ public/static/js/config.js
@@ -2,7 +2,7 @@ require.config({
 	baseUrl: "/static/js",
 	paths: {
 		jquery: "plugins/jquery.min",
-		underscore: "plugins/underscore-min.min",
+		underscore: "plugins/underscore-min",
 		self: "plugins/self"
 	}
 })
