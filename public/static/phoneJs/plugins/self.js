830b3bc032fb89116e431dab02a04ac4b14059fa
guohao <1403902918@qq.com>
2018-07-16 22:59:41 +0800

    解决方案

--- public/static/phoneJs/plugins/self.js
+++ public/static/phoneJs/plugins/self.js
@@ -1,13 +1,34 @@
 // 自定义 js 文件，放置自己的函数
 define(function () {
-	// 下标循环函数-以数学坐标轴的x轴坐标点为准
-	// 参数1：开始范围
-	// 参数2：结束范围
-	// 参数3：在开始和结束范围内循环的下标
-	function loop(d,a,c){c>0?c++:0;var b=c%(a-d+1);return c>0?b==0?a:d+b-1:b==0?d:a+b+1};
-
-
+	function parseURL(url) {
+	    var a =  document.createElement('a');
+	    a.href = url;
+	    return {
+	        source: url,
+	        protocol: a.protocol.replace(':',''),
+	        host: a.hostname,
+	        port: a.port||'80',
+	        query: a.search,
+	        params: (function(){
+	            var ret = {},
+	                seg = a.search.replace(/^\?/,'').split('&'),
+	                len = seg.length, i = 0, s;
+	            for (;i<len;i++) {
+	                if (!seg[i]) { continue; }
+	                s = seg[i].split('=');
+	                ret[s[0]] = s[1];
+	            }
+	            return ret;
+	        })(),
+	        file: (a.pathname.match(/\/([^\/?#]+)$/i) || [,''])[1],
+	        hash: a.hash.replace('#',''),
+	        path: a.pathname.replace(/^([^\/])/,'/$1'),
+	        relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [,''])[1],
+	        segments: a.pathname.replace(/^\//,'').split('/')
+	    };
+	}
+	
 	return {
-		loop: loop
+		parseURL: parseURL
 	}
 })
