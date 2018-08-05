44c19301a48116e2c8329331255ced93f6737355
guohao <1403902918@qq.com>
2018-08-06 01:04:17 +0800

    线上修改

--- public/static/phoneJs/config.js
+++ public/static/phoneJs/config.js
@@ -7,4 +7,18 @@ require.config({
 	}
 })
 
-
+// 全局补丁，新闻列表页，对内容的长度限制，防止内容过长出现在查看详情按钮下方
+require(['jquery'], function ($) {
+	$(function () {
+		$('.news-list p span').each(function () {
+			let lineNum = 43,
+				txt = $(this).text().trim(),
+				yu = txt.length % lineNum
+			;
+			if (yu > 7) {
+				txt = txt.slice(0, txt.length - (yu - 13));
+			}
+			$(this).text(txt + '...');
+		})
+	})
+})
